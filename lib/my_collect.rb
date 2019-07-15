802c4c43616bb969a8cd5cd03b666dfacc592e13
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-15 20:39:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+  counter = 0
+  collected = []
+  while counter < collection.length do
+    collected << yield(collection[counter])
+    counter += 1
+  end
+  collected
+end
 
