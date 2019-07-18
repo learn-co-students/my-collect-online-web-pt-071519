0590cc1018d8d47cb4232e51953b2f5d1d9f10ad
corbinarnett <corbin.c.arnett@gmail.com>
2019-07-17 21:50:48 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+require 'pry'
+def my_collect(array)
+  i = 0
+  collect_array = []
+  while i < array.length do 
+    collect_array << yield(array[i])
+    # binding.pry
+    i += 1
+  end
+  collect_array
+end
 
