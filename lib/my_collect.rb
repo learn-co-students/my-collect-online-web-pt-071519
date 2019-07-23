0e185789003a91d9020dec4a442fed942870f421
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-23 15:03:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+# this was my first try = unsuccessful!
+# def my_collect(languages)
+#   i = 0
+#   collection = []
+#   while i < collection.length
+#   collection << yield(array[i])
+#   i += 1
+#   my_collect(languages) do |lang| lang.upcase
+#     end
+#     collection
+# end
+# end
 
-
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
