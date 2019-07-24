0b681e4d9b5af3ba7c3dcdec1856a24704719fdc
Colleen Halpenny <cp.halpenny@gmail.com>
2019-07-24 05:46:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    final = []
+    while i < array.length
+      yield array[i]
+        final << yield(array[i])
+      i += 1
+    end
+    final
+  else
+    puts "Please enter a block"
+end
+end
